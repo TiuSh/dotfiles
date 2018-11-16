335c4d4fc0c22058fbe113fb7abfd3506401f82b
Mathieu Masy <mathieu@potloc.com>
2018-11-16 10:50:17 -0500

    [Oni] Add config files

---
+++ config.oni.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import * as Oni from "oni-api"
+
+// export const activate = (oni: Oni.Plugin.Api) => {
+// }
+
+// export const deactivate = (oni: Oni.Plugin.Api) => {
+// }
+
+export const configuration = {
+  // activate,
+  // deactivate,
+
+  // ONI
+  "oni.useDefaultConfig": false,
+  "oni.loadInitVim": "~/.config/oni/init.vim",
+
+  // UI
+  "ui.colorscheme": "onedark",
+  // "colors.background": "#073642",
+  // "colors.title.background": "#073642",
+
+  // Eitor
+  "editor.fontFamily": "Hack-Bold",
+  "editor.fontSize": "12px",
+
+  // Sidebar
+  "sidebar.enabled": false,
+
+  // Learning
+  "learning.enabled": true,
+
+  // Tabs
+  "tabs.mode": "native",
+
+  // Achievements
+  "achievements.enabled": false,
+
+  // Prettier
+  "oni.plugins.prettier": {
+    "formatOnSave": false,
+    "enabled": false
+  },
+
+
+  // Experimental
+  "experimental.indentLines.enabled": true
+}
