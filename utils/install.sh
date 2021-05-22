6707223ecba22dd3bc9d1b6a3581f8c98b916d54
Mathieu Masy <masy.mathieu@gmail.com>
2021-05-22 14:52:23 -0400

    Add unzip to utils

--- utils/install.sh
+++ utils/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Utils
-sudo apt install -y curl wget git htop bmon iftop xclip jq pavucontrol silversearcher-ag ripgrep
+sudo apt install -y curl wget git htop bmon iftop xclip jq pavucontrol silversearcher-ag ripgrep unzip
 
 # Setup max watches
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
