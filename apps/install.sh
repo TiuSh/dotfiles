d3fa7ef73711951bd5b5765aa41764e79644bf93
Mathieu Masy <mathieu@potloc.com>
2021-06-22 19:05:30 -0400

    WIP

--- apps/install.sh
+++ apps/install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
 
 # Firefox
-sudo apt install firefox
+sudo apt install -y firefox
 
 # Brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install -y brave-browser
 
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update
-sudo apt install google-chrome-stable
+sudo apt install -y google-chrome-stable
 
 # 1Password
 sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://downloads.1password.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
 sudo apt update
-sudo apt install 1password
+sudo apt install -y 1password
